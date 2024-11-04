346a1994aaf52b800792eaf9215867b51dad1247
Tonykhoueiry21 <tonykhoueiry21@gmail.com>
2024-11-04 09:43:43 -0500

    Update store.php

--- store.php
+++ store.php
@@ -1,6 +1,6 @@
 <?php
-$myfile = fopen("location.txt", "a");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET ["long"];
+$myfile = fopen("location.txt", "w");
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
